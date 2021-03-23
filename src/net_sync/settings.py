063828e0820bd74b40ff2970a49dbbd9b7b8b0f6
Lucas Lucyk <lucaslucyk@gmail.com>
2021-03-23 13:48:20 -0300

    update requirements

--- src/net_sync/settings.py
+++ src/net_sync/settings.py
@@ -14,7 +14,7 @@ import os
 import json
 
 # current version
-__version__ = "0.11.0"
+__version__ = "0.11.4"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
